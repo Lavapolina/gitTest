c5dc60d2d5ee68ebc4d32068b387ef0663d8f370
Polina <la-polya02@mail.ru>
2022-11-17 20:42:48 +0700

    Ввод1

--- pio4/pio4/Program.cs
+++ pio4/pio4/Program.cs
@@ -10,7 +10,8 @@ namespace piogi4
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Enter a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
         }
     }
 }

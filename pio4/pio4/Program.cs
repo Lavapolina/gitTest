eaef1d761490fce1b5e14b39b9ac37c335c99f08
Polina <la-polya02@mail.ru>
2022-11-17 20:45:02 +0700

    Сложение

--- pio4/pio4/Program.cs
+++ pio4/pio4/Program.cs
@@ -14,6 +14,7 @@ namespace piogi4
             int a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter b: ");
             int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"a + b = {a + b}");
         }
     }
 }

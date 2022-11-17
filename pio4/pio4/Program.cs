40e321c22e17241b69494b8e547299b888695f6b
Polina <la-polya02@mail.ru>
2022-11-17 20:47:59 +0700

    Умножение

--- pio4/pio4/Program.cs
+++ pio4/pio4/Program.cs
@@ -16,6 +16,7 @@ namespace piogi4
             int b = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"a + b = {a + b}");
             Console.WriteLine($"a - b = {a - b}");
+            Console.WriteLine($"a * b = {a * b}");
         }
     }
 }
